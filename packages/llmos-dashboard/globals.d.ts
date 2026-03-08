13f75375a813a1a50c37675473927b5950f786d3
boris-fouomene <borisfouomen14@gmail.com>
2026-03-08 09:40:08 +0100

    Refactor code structure for improved readability and maintainability

---
+++ packages/llmos-dashboard/globals.d.ts
@@ -0,0 +1,10 @@
+// Custom module declarations for CSS imports
+// Allows importing stylesheets as modules without type errors.
+
+declare module "*.css";
+declare module "*.scss";
+declare module "*.sass";
+declare module "*.less";
+
+declare module "*.module.css";
+declare module "*.module.scss";
